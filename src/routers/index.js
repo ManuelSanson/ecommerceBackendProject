5e72a838bf6c528220e3ad9360992f055615421a
Manuel Sanson <manu.sanson9@gmail.com>
2023-01-18 23:51:05 -0300

    products and carts db routers

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,3 +1,5 @@
 export { productsRouter } from './productsRouter.js';
 export { cartsRouter } from './cartRouter.js';
 export { viewsRouter } from './viewsRouter.js'
+export { productsDBRouter } from './productsDBRouter.js'
+export { cartsDBRouter } from './cartsDBRouter.js'
