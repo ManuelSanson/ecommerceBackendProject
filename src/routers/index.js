f0f3e435e9448c6576bd8f302b20bd9fe7207681
Manuel Sanson <manu.sanson9@gmail.com>
2023-01-19 19:39:25 -0300

    fix import

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,4 +2,4 @@ export { productsRouter } from './productsRouter.js';
 export { cartsRouter } from './cartRouter.js';
 export { viewsRouter } from './viewsRouter.js'
 export { productsDBRouter } from './productsDBRouter.js'
-export { cartsDBRouter } from './cartsDBRouter.js'
+export { cartsDBRouter } from './cartsDBRouter.js';
