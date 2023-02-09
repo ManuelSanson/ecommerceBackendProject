ce4bc4d772c7c2eebc03abc105fc3b3434c889ad
Manuel Sanson <manu.sanson9@gmail.com>
2023-02-09 17:40:53 -0300

    add greeting to user

--- src/routers/viewsRouter.js
+++ src/routers/viewsRouter.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { productManager } from '../dao/ManagersFS/index.js'
+import { productModel } from '../dao/models/productModel.js';
 
 export const viewsRouter = Router()
 
 const auth = (req, res, next) => {
     if (req.session?.user) return next()
 
-    return res.status(401).send(`Auth error. Debes <a href="/session/login">iniciar sesión</a>`)
+    return res.status(401).send(`Auth error. Debes <a href="/session/logins">iniciar sesión</a>`)
 }
 
 const adminAuth = (req, res, next) => {
@@ -16,17 +16,16 @@ const adminAuth = (req, res, next) => {
 }
 
 viewsRouter.get('/', auth, async (req, res) => {
-    const products = await productManager.getProducts()
+    const products = await productModel.find().lean().exec()
+    const user = req.session.user
 
-    res.render('home', {products})
+    res.render('home', {products, user})
 })
 
 viewsRouter.get('/realTimeProducts', adminAuth, (req, res) => {
-    
     res.render('realTimeProducts')
 })
 
 viewsRouter.get('/chat', auth, (req, res) => {
-    
     res.render('liveChat')
 })
