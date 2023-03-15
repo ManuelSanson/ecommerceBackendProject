1f2237ce28a295bf1fb1789658122d6f1bdf8ff5
Manuel Sanson <manu.sanson9@gmail.com>
2023-03-15 20:15:55 -0300

    update exports

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,4 @@
 export { productsRouter } from './productsRouter.js';
 export { cartsRouter } from './cartRouter.js';
-export { viewsRouter } from './viewsRouter.js'
-export { productsDBRouter } from './productsDBRouter.js'
-export { cartsDBRouter } from './cartsDBRouter.js';
+export { viewsRouter } from './viewsRouter.js';
 export { sessionRouter } from './sessionsRouter.js';
