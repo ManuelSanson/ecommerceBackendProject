736a84accc0201fa05cd56ab14a017302d344161
Manuel Sanson <manu.sanson9@gmail.com>
2023-04-11 18:36:00 -0300

    correct logger

--- src/middlewares/errors/errorsMiddleware.js
+++ src/middlewares/errors/errorsMiddleware.js
@@ -2,7 +2,7 @@ import { EErrors } from "../../services/errors/enums.js";
 import { logger } from '../../config/logger.js';
 
 export default (error, req, res, next) => {
-    log.error(error.cause);
+    logger.error(error.cause);
     switch (error.code) {
         case EErrors.INVALID_TYPES_ERROR:
             res.send({status: "error", error: error.name})            
