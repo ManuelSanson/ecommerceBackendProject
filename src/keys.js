5b91fe3368ce405d41d1ec5fc62fa407ec2db5e8
Manuel Sanson <manu.sanson9@gmail.com>
2023-05-02 12:36:23 -0300

    update keys

--- src/keys.js
+++ src/keys.js
@@ -2,6 +2,5 @@ export const keys = {
     google: {
         clientID: '671066869917-clik55eifs4ju1oornj0oqip3qs6gs9g.apps.googleusercontent.com',
         clientSecret: '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'
-    },
-    mongoURI: 'mongodb+srv://ManuelSanson:5hRX9r2eJXDzXO8f@cluster0.w3fwwwq.mongodb.net/?retryWrites=true&w=majority'
+    }
 }
