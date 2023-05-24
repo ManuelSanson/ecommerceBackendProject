303f4448fecaf00a71f7b3cfba96a4095af04657
Manuel Sanson <manu.sanson9@gmail.com>
2023-05-23 22:57:28 -0300

    update index

--- src/repository/index.js
+++ src/repository/index.js
@@ -5,23 +5,28 @@ import MessageRepository from './messagesRepository.js';
 import ProductRepository from './productsRepository.js';
 import UserRepository from './usersRepository.js';
 import TicketRepository from './ticketsRepository.js';
+import Carts from '../DAO/mongo/cartsMongo.js';
+import Products from '../DAO/mongo/productsMongo.js';
+import Users from '../DAO/mongo/usersMongo.js';
+import Messages from '../DAO/mongo/messagesMongo.js';
+import Tickets from '../DAO/mongo/ticketsMongo.js';
 
-export let Carts
-export let Products
-export let Users
-export let Messages
-export let Tickets
+// export let Carts
+// export let Products
+// export let Users
+// export let Messages
+// export let Tickets
 
-const { default: CartsMongo} = await import('../DAO/mongo/cartsMongo.js')
-Carts = CartsMongo
-const { default: ProductsMongo} = await import('../DAO/mongo/productsMongo.js')
-Products = ProductsMongo
-const { default: UsersMongo} = await import('../DAO/mongo/usersMongo.js')
-Users = UsersMongo
-const { default: MessagesMongo} = await import('../DAO/mongo/messagesMongo.js')
-Messages = MessagesMongo
-const { default: TicketsMongo} = await import('../DAO/mongo/ticketsMongo.js')
-Tickets = TicketsMongo
+// const { default: CartsMongo} = await import('../DAO/mongo/cartsMongo.js')
+// Carts = CartsMongo
+// const { default: ProductsMongo} = await import('../DAO/mongo/productsMongo.js')
+// Products = ProductsMongo
+// const { default: UsersMongo} = await import('../DAO/mongo/usersMongo.js')
+// Users = UsersMongo
+// const { default: MessagesMongo} = await import('../DAO/mongo/messagesMongo.js')
+// Messages = MessagesMongo
+// const { default: TicketsMongo} = await import('../DAO/mongo/ticketsMongo.js')
+// Tickets = TicketsMongo
 
 export const CartService = new CartRepository(new Carts())
 export const MessageService = new MessageRepository(new Messages())
